36a5fd2cf50e1f2d0723c50549fe4af942db8940
Joshua <Joshuahale829@gmail.com>
2024-04-02 11:58:52 -0500

    Wrote out models folder and its associated files, and  fixed a typo in the package.json

---
+++ models/User.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const { reactionSchema } = require('./Reaction');
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+@.+\..+/, 'Please enter a valid email address']
+  },
+  thoughts: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Thought'
+  }],
+  friends: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }]
+});
+
+userSchema.virtual('friendCount').get(function() {
+  return this.friends.length;
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
